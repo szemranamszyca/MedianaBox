5e67b893dbc0d55497c1c3657c70aa5dd69a4487
Arkadiusz Konior <arekkonior@MacBook-Pro-Mikolaj.local>
2019-01-04 19:12:08 +0300

    simplify insert method

--- src/Calculator.cpp
+++ src/Calculator.cpp
@@ -10,22 +10,15 @@ Calculator::Calculator(std::unique_ptr<containers::ISimpleContainer> container) 
 
 void Calculator::insert(int value)
 {
-    if (container_->size() == 0)
+    for(std::size_t i = 0; i < container_->size(); i++)
     {
-        container_->push_back(value);
-    }
-    else
-    {
-        for(std::size_t i = 0; i < container_->size(); i++)
+        if (value < (*container_)[i])
         {
-            if (value < (*container_)[i])
-            {
-                container_->insert(value, i);
-                return;
-            }
+            container_->insert(value, i);
+            return;
         }
-        container_->push_back(value);
     }
+    container_->push_back(value);
 }
 
 double Calculator::calculate() const
